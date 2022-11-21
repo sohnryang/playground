facd40c9f33ad3bfdb227393e9cce416cc418f87
Ryang Sohn <loop.infinitely@gmail.com>
2022-11-21 13:47:22 +0900

    Add json message parsing

--- aws/sqsclient/main.go
+++ aws/sqsclient/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
+
+type Data struct {
+	BoolData bool   `json:"bool"`
+	IntData  int    `json:"int"`
+	StrData  string `json:"str"`
+}
 
 func must(err error) {
 	if err != nil {
@@ -41,7 +48,29 @@ func main() {
 	log.Println("SendMessage result:")
 	log.Println(sendResult)
 
+	d := Data{
+		BoolData: true,
+		IntData:  42,
+		StrData:  "hell world!",
+	}
+	b, err := json.Marshal(d)
+	must(err)
+	sendResult, err = svc.SendMessage(&sqs.SendMessageInput{
+		MessageBody: aws.String(string(b)),
+		QueueUrl:    u,
+	})
+	must(err)
+	log.Println("SendMessage result:")
+	log.Println(sendResult)
+
 	recvResult, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl: u,
+	})
+	must(err)
+	log.Println("ReceiveMessage result:")
+	log.Println(recvResult)
+
+	recvResult, err = svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 		QueueUrl: u,
 	})
 	must(err)
