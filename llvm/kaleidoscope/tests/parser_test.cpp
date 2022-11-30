1f70a359d5e8fa52e81120426f814d1185daf7e2
Ryang Sohn <loop.infinitely@gmail.com>
2022-11-30 14:49:03 +0900

    Add tests for parser

---
+++ llvm/kaleidoscope/tests/parser_test.cpp
@@ -0,0 +1,34 @@
+#include <ast.h>
+#include <parser.h>
+
+#include <gtest/gtest.h>
+
+#include <sstream>
+
+TEST(ParserTest, Int) {
+  auto sb = new std::stringbuf("42");
+  Parser parser(sb);
+  EXPECT_NO_THROW(parser.parse_int());
+  delete sb;
+}
+
+TEST(ParserTest, Float) {
+  auto sb = new std::stringbuf("2.71828");
+  Parser parser(sb);
+  EXPECT_NO_THROW(parser.parse_float());
+  delete sb;
+}
+
+TEST(ParserTest, Prototype) {
+  auto sb = new std::stringbuf("func(a b c)");
+  Parser parser(sb);
+  EXPECT_NO_THROW(parser.parse_proto());
+  delete sb;
+}
+
+TEST(ParserTest, Expr) {
+  auto sb = new std::stringbuf("a*a + b*b - c*c");
+  Parser parser(sb);
+  EXPECT_NO_THROW(parser.parse_expr());
+  delete sb;
+}
