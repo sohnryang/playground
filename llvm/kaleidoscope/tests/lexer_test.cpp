bfaeece5a61c1c511f1b2de7e12498661fafabda
Ryang Sohn <loop.infinitely@gmail.com>
2022-12-02 17:03:55 +0900

    Add EOF checks to lexer tests

--- llvm/kaleidoscope/tests/lexer_test.cpp
+++ llvm/kaleidoscope/tests/lexer_test.cpp
@@ -8,12 +8,14 @@
 TEST(LexerTest, Def) {
   Lexer lexer("def");
   EXPECT_EQ(lexer.get_token(), Token(TokenKind::kDef, "def"));
+  EXPECT_EQ(lexer.get_token(), Token(TokenKind::kEof, ""));
 }
 
 TEST(LexerTest, Number) {
   Lexer lexer("42 3.141592");
   EXPECT_EQ(lexer.get_token(), Token(TokenKind::kInt, "42"));
   EXPECT_EQ(lexer.get_token(), Token(TokenKind::kFloat, "3.141592"));
+  EXPECT_EQ(lexer.get_token(), Token(TokenKind::kEof, ""));
 }
 
 TEST(LexerTest, FunctionDef) {
@@ -23,4 +25,5 @@ TEST(LexerTest, FunctionDef) {
   EXPECT_EQ(lexer.get_token(), Token(TokenKind::kMisc, "("));
   EXPECT_EQ(lexer.get_token(), Token(TokenKind::kMisc, ")"));
   EXPECT_EQ(lexer.get_token(), Token(TokenKind::kInt, "0"));
+  EXPECT_EQ(lexer.get_token(), Token(TokenKind::kEof, ""));
 }
