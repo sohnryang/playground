4a8e0fbc59b5075bb138425aa82a56a71f24567e
Ryang Sohn <loop.infinitely@gmail.com>
2022-12-02 20:35:31 +0900

    Add AST dump

--- llvm/kaleidoscope/src/kalc.cpp
+++ llvm/kaleidoscope/src/kalc.cpp
@@ -1,28 +1,65 @@
-#include <boost/program_options.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/variables_map.hpp>
+#include <parser.h>
 
+#include <exception>
+#include <fstream>
+#include <ios>
 #include <iostream>
+#include <memory>
+#include <sstream>
 #include <string>
+
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+
+#include <boost/exception/all.hpp>
+#include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
   po::options_description desc("LLVM Kaleidoscope tutorial implementation");
   desc.add_options()("help", "produce help message")(
-      "emit-ir", po::value<std::string>(),
-      "emit LLVM IR")("input,i", po::value<std::string>(), "input file");
+      "emit-ir", po::value<std::string>(), "emit LLVM IR")("dump-ast",
+                                                           "print ast info");
+  po::options_description hidden("Hidden");
+  hidden.add_options()("input-file", "input file");
+  po::positional_options_description pos;
+  pos.add("input-file", 1);
   po::variables_map vmap;
-  po::store(po::parse_command_line(argc, argv, desc), vmap);
+  po::options_description cmdline_options;
+  cmdline_options.add(desc).add(hidden);
+  try {
+    po::store(po::command_line_parser(argc, argv)
+                  .options(cmdline_options)
+                  .positional(pos)
+                  .run(),
+              vmap);
+  } catch (boost::exception const &err) {
+    std::cerr << boost::diagnostic_information(err, true) << std::endl;
+    return 1;
+  }
   po::notify(vmap);
 
   if (vmap.count("help")) {
     std::cout << desc << std::endl;
     return 0;
   }
-  if (vmap.count("input")) {
-    return 0;
+  if (!vmap.count("input-file")) {
+    std::cerr << "No input specified" << std::endl;
+    return 1;
   }
-  std::cout << "Running kalc in interpreter mode" << std::endl;
+
+  std::ifstream infile(vmap["input-file"].as<std::string>());
+  std::stringstream buf;
+  buf << infile.rdbuf();
+  auto parser = Parser(buf.str());
+  auto ast = parser.parse_all();
+
+  if (vmap.count("dump-ast")) {
+    for (auto &root : ast)
+      std::cout << root->to_string() << std::endl;
+  }
+
   return 0;
 }
