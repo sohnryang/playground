6bb9c8d1063431c2ccf578cf3136d46f87461fe4
Ryang Sohn <loop.infinitely@gmail.com>
2022-12-06 14:45:28 +0900

    Add AST printer for printing AST

---
+++ llvm/kaleidoscope/include/ast_printer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <ast.h>
+
+#include <memory>
+#include <string>
+
+class ASTPrinter {
+public:
+  std::string operator()(const LiteralExprNode<int> &node) const;
+  std::string operator()(const LiteralExprNode<double> &node) const;
+  std::string operator()(const VariableExprNode &node) const;
+  std::string operator()(const std::unique_ptr<BinaryExprNode> &node) const;
+  std::string operator()(const std::unique_ptr<CallExprNode> &node) const;
+  std::string operator()(const PrototypeNode &node) const;
+  std::string operator()(const FunctionNode &node) const;
+};
