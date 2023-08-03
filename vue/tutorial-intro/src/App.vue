03b6090a766b05ac9890fe82b1aa4fac65a07329
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-03 15:40:25 +0900

    feat(vue-tutorial): add class and style bindings

--- vue/tutorial-intro/src/App.vue
+++ vue/tutorial-intro/src/App.vue
@@ -23,6 +23,14 @@ const challenge = reactive({
 const isWebChallenge = computed(() => {
   return challenge.tags.includes("web") ? "Yes" : "No"
 })
+
+const isTextEnabled = ref<Boolean>(true);
+
+const enableToggleButtonText = computed(() => {
+  return isTextEnabled.value ? "Disable" : "Enable"
+})
+
+const fontSize = ref(10)
 </script>
 
 <template>
@@ -37,12 +45,35 @@ const isWebChallenge = computed(() => {
   <span v-if="count % 2 == 1">The <code>count</code> is odd!</span>
 
   <h2>Reactivity</h2>
-  <button @click="reactiveCount++">Increment <code>reactiveCount</code>: {{ reactiveCount }}</button>
-  <button @click="reactiveCount = 0">Reset <code>reactiveCount</code></button>
+  <button @click="reactiveCount++">
+    Increment <code>reactiveCount</code>: {{ reactiveCount }}
+  </button>
+  <button @click="reactiveCount = 0">
+    Reset <code>reactiveCount</code>
+  </button>
   <br>
-  <button @click="nonReactiveCount++"> Increment <code>nonReactiveCount</code>: {{ nonReactiveCount }}</button>
+  <button @click="nonReactiveCount++">
+    Increment <code>nonReactiveCount</code>: {{ nonReactiveCount }}
+  </button>
 
   <h2>Computed Props</h2>
   <p>Challenge name: {{ challenge.name }}</p>
   <p>Is it a web challenge? {{ isWebChallenge }}</p>
+
+  <h2>Class and Style Bindings</h2>
+  <p :class="{ 'disabled-text': !isTextEnabled }">Some text</p>
+  <button @click="isTextEnabled = !isTextEnabled">
+    {{ enableToggleButtonText }}
+  </button>
+  <p :style="{ 'font-size': fontSize + 'px' }">
+    Resizable text (size: {{ fontSize }}px)
+  </p>
+  <button @click="fontSize += 10">Increase <code>fontSize</code></button>
+  <button @click="fontSize -= 10">Decrease <code>fontSize</code></button>
 </template>
+
+<style scoped>
+.disabled-text {
+  color: lightgray;
+}
+</style>
