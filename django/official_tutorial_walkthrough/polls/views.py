78fc3e5b61089929a2d6ecb384760c579a33fb11
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-20 16:22:33 +0900

    feat(django_tutorial): add question related views

--- django/official_tutorial_walkthrough/polls/views.py
+++ django/official_tutorial_walkthrough/polls/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+
+from .models import Question
 
 
-def index(request):
-    return HttpResponse(b"Hell world!")
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"Question with id {question_id}.".encode())
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"Results of question with id {question_id}".encode())
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"Voting on question with id {question_id}".encode())
