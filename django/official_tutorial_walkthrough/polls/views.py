67aabb98a096059705b8ed051703aedf0ea663f6
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-20 16:32:51 +0900

    feat(django_tutorial): use templates

--- django/official_tutorial_walkthrough/polls/views.py
+++ django/official_tutorial_walkthrough/polls/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 
 def index(request: HttpRequest) -> HttpResponse:
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse(f"Question with id {question_id}.".encode())
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request: HttpRequest, question_id: int) -> HttpResponse:
