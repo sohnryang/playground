65586f56bab64c89c0f1160977ff66caac4b4a35
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-20 12:57:13 +0900

    feat(django_tutorial): init django project

---
+++ django/official_tutorial_walkthrough/official_tutorial_walkthrough/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for official_tutorial_walkthrough project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "official_tutorial_walkthrough.settings"
+)
+
+application = get_asgi_application()
---
+++ django/official_tutorial_walkthrough/official_tutorial_walkthrough/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for official_tutorial_walkthrough project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "official_tutorial_walkthrough.settings"
+)
+
+application = get_wsgi_application()
