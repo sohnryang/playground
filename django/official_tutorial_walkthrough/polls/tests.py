6e38ceb8dd2ad9549a3abe6ac998d12694418b17
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-27 12:36:34 +0900

    test(django_tutorial): add a simple test

--- django/official_tutorial_walkthrough/polls/tests.py
+++ django/official_tutorial_walkthrough/polls/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Question
+
+
+class QuestionModuleTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_questions = Question(pub_date=time)
+        self.assertFalse(future_questions.was_published_recently())
