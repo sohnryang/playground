862ffe4e127453e5dee25979489c9694fdc6e649
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-27 12:12:13 +0900

    feat(django_tutorial): connect views to templates

--- django/official_tutorial_walkthrough/polls/views.py
+++ django/official_tutorial_walkthrough/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -16,8 +17,23 @@ def detail(request: HttpRequest, question_id: int) -> HttpResponse:
 
 
 def results(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse(f"Results of question with id {question_id}".encode())
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse(f"Voting on question with id {question_id}".encode())
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])  # type: ignore
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "No choice selected"},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))  # type: ignore
+        )
