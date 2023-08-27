f6f814f5b4f713017cad4a8f361c5ef5bfab53a9
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-27 13:20:39 +0900

    test(django_tutorial): add tests for detail view

--- django/official_tutorial_walkthrough/polls/tests.py
+++ django/official_tutorial_walkthrough/polls/tests.py
@@ -31,6 +31,20 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(response.context["latest_question_list"], [])
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))  # type: ignore
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))  # type: ignore
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 class QuestionModuleTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
