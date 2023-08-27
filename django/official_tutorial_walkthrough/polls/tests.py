01b0a32edf4af3e9aa4752678350a3e62bb3bc9f
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-27 12:41:08 +0900

    test(django_tutorial): add more tests

--- django/official_tutorial_walkthrough/polls/tests.py
+++ django/official_tutorial_walkthrough/polls/tests.py
@@ -11,3 +11,13 @@ class QuestionModuleTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_questions = Question(pub_date=time)
         self.assertFalse(future_questions.was_published_recently())
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_questions = Question(pub_date=time)
+        self.assertFalse(old_questions.was_published_recently())
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_questions = Question(pub_date=time)
+        self.assertTrue(recent_questions.was_published_recently())
