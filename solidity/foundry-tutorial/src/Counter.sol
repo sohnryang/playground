d1a3d63e2409f872bbaa46f0a01303e2e76828bf
Ryang Sohn <loop.infinitely@gmail.com>
2024-01-12 17:13:48 +0900

    feat(solidity.foundry-tutorial): add events

--- solidity/foundry-tutorial/src/Counter.sol
+++ solidity/foundry-tutorial/src/Counter.sol
@@ -8,14 +8,19 @@ contract Counter {
         number = newNumber;
     }
 
+    event Incremented(uint256 newNumber);
+
     function increment() public {
         number++;
+        emit Incremented(number);
     }
 
     error NumberIsZero();
+    event Decremented(uint256 newNumber);
 
     function derecrement() public {
         if (number == 0) revert NumberIsZero();
         number--;
+        emit Decremented(number);
     }
 }
