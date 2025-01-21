ba22e0c85f4541c14dc5414ec390cff24808154f
Ryang Sohn <loop.infinitely@gmail.com>
2025-01-21 11:55:15 +0900

    feat(simd.histogram): simplify implementation
    Still slower than scalar

--- simd/histogram/driver.c
+++ simd/histogram/driver.c
@@ -13,28 +13,15 @@ void kernel_scalar(const uint8_t *dist, int out[256], int n) {
 
 void kernel_avx512(const uint8_t *dist, int out[256], int n) {
   const __m512i ONES = _mm512_set1_epi32(1);
-  const __m512i BITCOUNTSMINONE = _mm512_set1_epi32(31);
-  const __m512i NEGONES = _mm512_set1_epi32(-1);
   int i;
   for (i = 0; i + 16 <= n; i += 16) {
     const __m128i x = _mm_load_epi32(&dist[i]);
     const __m512i x_epi32 = _mm512_cvtepu8_epi32(x);
     const __m512i conflicts = _mm512_conflict_epi32(x_epi32);
-    const __m512i conflicts_lzcnt = _mm512_lzcnt_epi32(conflicts);
+    const __m512i conflicts_popcnt = _mm512_popcnt_epi32(conflicts);
     const __m512i out_old = _mm512_i32gather_epi32(x_epi32, out, 4);
-    __m512i msb_pos = _mm512_sub_epi32(BITCOUNTSMINONE, conflicts_lzcnt);
-    __m512i element_counts = ONES;
-    __mmask16 conflicts_mask = _mm512_test_epi32_mask(conflicts, conflicts);
-    while (conflicts_mask) {
-      const __m512i conflicting_counts = _mm512_maskz_permutexvar_epi32(
-          conflicts_mask, msb_pos, element_counts);
-      msb_pos = _mm512_mask_permutexvar_epi32(msb_pos, conflicts_mask, msb_pos,
-                                              msb_pos);
-      element_counts = _mm512_mask_add_epi32(
-          element_counts, conflicts_mask, element_counts, conflicting_counts);
-      conflicts_mask = _mm512_cmpneq_epi32_mask(msb_pos, NEGONES);
-    }
-    const __m512i out_next = _mm512_add_epi32(out_old, element_counts);
+    const __m512i out_next =
+        _mm512_add_epi32(out_old, _mm512_add_epi32(conflicts_popcnt, ONES));
     _mm512_i32scatter_epi32(out, x_epi32, out_next, 4);
   }
   for (; i < n; i++)
