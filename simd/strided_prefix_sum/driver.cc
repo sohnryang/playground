8f7154d681115c2c180d869aa475414e0ef56e62
Ryang Sohn <loop.infinitely@gmail.com>
2025-02-01 15:21:22 +0900

    refactor(simd.strided_prefix_sum): pass function as template arg

--- simd/strided_prefix_sum/driver.cc
+++ simd/strided_prefix_sum/driver.cc
@@ -12,90 +12,65 @@ constexpr size_t MULTIPLIER = 4;
 constexpr size_t RANGE_START = 1024;
 constexpr size_t RANGE_END = 64 * 1024 * 1024;
 
-template <typename T, int S> static void BM_scalar(benchmark::State &state) {
+template <typename T, void (*F)(T *, int)>
+static void BM(benchmark::State &state) {
   T *arr = new T[state.range()];
   for (int i = 0; i < state.range(); i++)
     arr[i] = std::rand();
 
   for (auto _ : state) {
-    kernel_scalar<T, S>(arr, state.range());
+    F(arr, state.range());
     benchmark::DoNotOptimize(arr);
     benchmark::ClobberMemory();
   }
   delete[] arr;
 }
-BENCHMARK(BM_scalar<float, 2>)
+BENCHMARK(BM<float, kernel_scalar<float, 2>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_scalar<float, 3>)
+BENCHMARK(BM<float, kernel_scalar<float, 3>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_scalar<float, 4>)
+BENCHMARK(BM<float, kernel_scalar<float, 4>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_scalar<uint32_t, 2>)
+BENCHMARK(BM<uint32_t, kernel_scalar<uint32_t, 2>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_scalar<uint32_t, 3>)
+BENCHMARK(BM<uint32_t, kernel_scalar<uint32_t, 3>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_scalar<uint32_t, 4>)
+BENCHMARK(BM<uint32_t, kernel_scalar<uint32_t, 4>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-
-template <typename T, int S> static void BM_autovec(benchmark::State &state) {
-  T *arr = new T[state.range()];
-  for (int i = 0; i < state.range(); i++)
-    arr[i] = std::rand();
-
-  for (auto _ : state) {
-    kernel_autovec<T, S>(arr, state.range());
-    benchmark::DoNotOptimize(arr);
-    benchmark::ClobberMemory();
-  }
-  delete[] arr;
-}
-BENCHMARK(BM_autovec<float, 2>)
+BENCHMARK(BM<float, kernel_autovec<float, 2>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_autovec<float, 3>)
+BENCHMARK(BM<float, kernel_autovec<float, 3>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_autovec<float, 4>)
+BENCHMARK(BM<float, kernel_autovec<float, 4>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_autovec<uint32_t, 2>)
+BENCHMARK(BM<uint32_t, kernel_autovec<uint32_t, 2>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_autovec<uint32_t, 3>)
+BENCHMARK(BM<uint32_t, kernel_autovec<uint32_t, 3>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_autovec<uint32_t, 4>)
+BENCHMARK(BM<uint32_t, kernel_autovec<uint32_t, 4>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-
-template <typename T, int S> static void BM_avx512(benchmark::State &state) {
-  T *arr = new T[state.range()];
-  for (int i = 0; i < state.range(); i++)
-    arr[i] = std::rand();
-
-  for (auto _ : state) {
-    kernel_avx512<T, S>(arr, state.range());
-    benchmark::DoNotOptimize(arr);
-    benchmark::ClobberMemory();
-  }
-  delete[] arr;
-}
-BENCHMARK(BM_avx512<float, 2>)
+BENCHMARK(BM<float, kernel_avx512<float, 2>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_avx512<float, 4>)
+BENCHMARK(BM<float, kernel_avx512<float, 4>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_avx512<uint32_t, 2>)
+BENCHMARK(BM<uint32_t, kernel_avx512<uint32_t, 2>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
-BENCHMARK(BM_avx512<uint32_t, 4>)
+BENCHMARK(BM<uint32_t, kernel_avx512<uint32_t, 4>>)
     ->RangeMultiplier(MULTIPLIER)
     ->Range(RANGE_START, RANGE_END);
 
